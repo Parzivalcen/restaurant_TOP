f7d89c6815ece2cc6d4d04dca02074e92e7b57cc
David Centurion <davidcenwork@gmail.com>
2022-06-13 05:45:47 -0400

    add blocks for page blocks

--- dist/main.js
+++ dist/main.js
@@ -9,24 +9,100 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/homeIcon.svg":
+/*!**************************!*\
+  !*** ./src/homeIcon.svg ***!
+  \**************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module parse failed: Unexpected token (1:0)\\nYou may need an appropriate loader to handle this file type, currently no loaders are configured to process this file. See https://webpack.js.org/concepts#loaders\\n> <svg xmlns=\\\"http://www.w3.org/2000/svg\\\" width=\\\"24\\\" height=\\\"24\\\" viewBox=\\\"0 0 24 24\\\"><path d=\\\"M20 7.093v-5.093h-3v2.093l3 3zm4 5.907l-12-12-12 12h3v10h7v-5h4v5h7v-10h3zm-5 8h-3v-5h-8v5h-3v-10.26l7-6.912 7 6.99v10.182z\\\"/></svg>\");\n\n//# sourceURL=webpack://restaurant_top/./src/homeIcon.svg?");
+
+/***/ }),
+
 /***/ "./src/index.js":
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ (() => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("// Home elements\r\nhome = () =>{\r\n  const body = document.body\r\n  \r\n  const container = document.createElement('div');\r\n  container.classList.add('container', 'container--home');\r\n  // NAV BAR //\r\n  const navbar = document.createElement('div');\r\n  navbar.classList.add('nav');\r\n  container.append(navbar)\r\n\r\n\r\n\r\n  body.append(container);\r\n}\r\n\r\nhome();\r\n\n\n//# sourceURL=webpack://restaurant_top/./src/index.js?");
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _homeIcon_svg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homeIcon.svg */ \"./src/homeIcon.svg\");\n/* harmony import */ var _homeIcon_svg__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_homeIcon_svg__WEBPACK_IMPORTED_MODULE_0__);\n\r\nconst body = document.body;\r\n// Icon\r\nconst homeIcon = new Image ();\r\nhomeIcon.src = (_homeIcon_svg__WEBPACK_IMPORTED_MODULE_0___default());\r\n\r\n// Main\r\nconst main = () => {\r\n  const main = document.createElement('main');\r\n  body.appendChild(main);\r\n}\r\n\r\n// Navbar\r\nconst nav = () => {\r\n  const navbar = document.createElement('div');\r\n  navbar.classList.add('nav');\r\n  navbar.innerHTML = `\r\n  <img src= alt=\"home\">\r\n  <div class=\"tabs\">\r\n    <h2>Menu</h2>\r\n    <h2>About</h2>\r\n  </div>`;\r\n  body.append(navbar);\r\n}\r\n\r\n// Home elements\r\nconst home = () =>{\r\n  const container = document.createElement('div');\r\ncontainer.classList.add('container', 'container--home');\r\nbody.appendChild(container);\r\n  }\r\nnav();\r\nhome();\r\n\n\n//# sourceURL=webpack://restaurant_top/./src/index.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
 /******/ 	
 /******/ })()
 ;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,33 @@
-// Home elements
-home = () =>{
-  const body = document.body
-  
-  const container = document.createElement('div');
-  container.classList.add('container', 'container--home');
-  // NAV BAR //
-  const navbar = document.createElement('div');
-  navbar.classList.add('nav');
-  container.append(navbar)
+import hIcon from './homeIcon.svg';
+const body = document.body;
+// Icon
+const homeIcon = new Image ();
+homeIcon.src = hIcon;
 
-
-
-  body.append(container);
+// Main
+const main = () => {
+  const main = document.createElement('main');
+  body.appendChild(main);
 }
 
+// Navbar
+const nav = () => {
+  const navbar = document.createElement('div');
+  navbar.classList.add('nav');
+  navbar.innerHTML = `
+  <img src= alt="home">
+  <div class="tabs">
+    <h2>Menu</h2>
+    <h2>About</h2>
+  </div>`;
+  body.append(navbar);
+}
+
+// Home elements
+const home = () =>{
+  const container = document.createElement('div');
+container.classList.add('container', 'container--home');
+body.appendChild(container);
+  }
+nav();
 home();
